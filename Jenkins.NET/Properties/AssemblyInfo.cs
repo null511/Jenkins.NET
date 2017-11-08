59512a85a9742771ff0d027cbcf7906f0586ee0b
JoshuaMiller <admin@joshuamiller.info>
2017-11-08 02:14:40 -0500

    version

--- Jenkins.NET/Properties/AssemblyInfo.cs
+++ Jenkins.NET/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
 [assembly: AssemblyTitle("Jenkins.NET")]
 [assembly: AssemblyDescription("C# .NET wrapper for Jenkins HTTP/REST API.")]
 [assembly: AssemblyConfiguration("")]
