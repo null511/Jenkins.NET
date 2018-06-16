463039ae2f062eb6b2c522e252eb1d98c363e091
JoshuaMiller <admin@joshuamiller.info>
2018-06-16 13:50:21 -0400

    photon pipeline

--- Jenkins.NET/Properties/AssemblyInfo.cs
+++ Jenkins.NET/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("0.5.0")]
+[assembly: AssemblyFileVersion("0.5.0")]
 [assembly: AssemblyTitle("Jenkins.NET")]
 [assembly: AssemblyDescription("C# .NET wrapper for Jenkins HTTP/REST API.")]
 [assembly: AssemblyConfiguration("")]
