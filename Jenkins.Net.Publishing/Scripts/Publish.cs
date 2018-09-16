54278856f29c24153f9ffa7a6021e74bfa4cc68e
JoshuaMiller <admin@joshuamiller.info>
2018-09-16 15:56:51 -0400

    publish path fix

--- Jenkins.Net.Publishing/Scripts/Publish.cs
+++ Jenkins.Net.Publishing/Scripts/Publish.cs
@@ -23,7 +23,7 @@ namespace Jenkins.NET.Publishing.Scripts
             };
             nugetCore.Initialize();
 
-            var packageDir = Path.Combine(Context.ContentDirectory, "PublishPackage");
+            var packageDir = Path.Combine(Context.BinDirectory, "PublishPackage");
 
             var packageFilename = Directory
                 .GetFiles(packageDir, "jenkinsnet.*.nupkg")
