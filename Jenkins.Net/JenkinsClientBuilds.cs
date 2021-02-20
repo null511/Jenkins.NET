3fcb0af28ada2554740738e929d407d99174b9ec
Benedikt Göpfert <benedikt@beardshaker.com>
2021-02-20 03:26:21 +0100

    add: BuildStop command

--- Jenkins.Net/JenkinsClientBuilds.cs
+++ Jenkins.Net/JenkinsClientBuilds.cs
@@ -224,5 +224,42 @@ namespace JenkinsNET
             }
         }
     #endif
+        
+        /// <summary>
+        /// Stops an active Jenkins Build.
+        /// </summary>
+        /// <param name="jobName">The name of the Job.</param>
+        /// <param name="buildNumber">The number of the build.</param>
+        /// <exception cref="JenkinsJobGetBuildException"></exception>
+        public void Stop(string jobName, string buildNumber)
+        {
+            try {
+                var cmd = new BuildStopCommand(client, jobName, buildNumber);
+                cmd.Run();
+            }
+            catch (Exception error) {
+                throw new JenkinsNetException($"Failed to stop build #{buildNumber} of Jenkins Job '{jobName}'!", error);
+            }
+        }
+
+    #if NET_ASYNC
+        /// <summary>
+        /// Gets information describing a Jenkins Job Build asynchronously.
+        /// </summary>
+        /// <param name="jobName">The name of the Job.</param>
+        /// <param name="buildNumber">The number of the build.</param>
+        /// <param name="token">An optional token for aborting the request.</param>
+        /// <exception cref="JenkinsJobGetBuildException"></exception>
+        public async Task StopAsync(string jobName, string buildNumber, CancellationToken token = default)
+        {
+            try {
+                var cmd = new BuildStopCommand(client, jobName, buildNumber);
+                await cmd.RunAsync(token);
+            }
+            catch (Exception error) {
+                throw new JenkinsNetException($"Failed to stop build #{buildNumber} of Jenkins Job '{jobName}'!", error);
+            }
+        }
+    #endif
     }
 }
